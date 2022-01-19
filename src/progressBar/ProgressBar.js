fc5fe0c6e13e8f49a0db00c3df8c8057cc6c803a
ikismail <ikismail7@gmail.com>
2022-01-20 01:39:13 +0530

    Migrated Class Components to functional components

--- src/progressBar/ProgressBar.js
+++ src/progressBar/ProgressBar.js
@@ -1,86 +1,79 @@
-import React, {Component} from 'react';
-import {
-    View,
-    TouchableOpacity,
-    ViewPropTypes, Text} from 'react-native';
-import PropTypes from 'prop-types';
-import { styles } from './Styles';
-import SingleBar from './SingleBar';
+import PropTypes from "prop-types";
+import React from "react";
+import { Text, TouchableOpacity, View, ViewPropTypes } from "react-native";
+import SingleBar from "./SingleBar";
+import { styles } from "./Styles";
 
-export default class ProgressBar extends Component {
-    constructor(props) {
-      super(props);
-      let total = this.getCoveredBar(props);
-      this.state = {
-        total
-      };
-      this.getSingleProgressBar = this.getSingleProgressBar.bind(this);
-    }
-  
-    shouldComponentUpdate(nextProps, nextState){
-      let shouldUpdate = this.props.arrayOfProgressObjects != nextProps.arrayOfProgressObjects
-      return (
-        shouldUpdate
-      )
-    }
-  
-    UNSAFE_componentWillReceiveProps(nextProps) {
-      if(this.props.arrayOfProgressObjects != nextProps.arrayOfProgressObjects){
-        let total = this.getCoveredBar(nextProps);
-        this.setState({
-          total
-        })
-      }
-    }
-  
-    getSingleProgressBar(item,index){
-      let {onStartProgressStyle,onEndProgressStyle,arrayOfProgressObjects,textStyle} = this.props;
-      let totalProgress = parseInt('' + this.state.total);
-      return (
-        <SingleBar
-          key={'' + index}
-          {...item}
-          onEndProgressStyle={onEndProgressStyle}
-          onStartProgressStyle={onStartProgressStyle}
-          textStyle={textStyle}
-          isLast={arrayOfProgressObjects.length==index+1 && totalProgress >= 1}
-          index={index}
-        />
-      )
-    }
-  
-    getCoveredBar(props){
-      let readings = props.arrayOfProgressObjects;
-      return readings.reduce((total,bar) => {return total + bar.value;},0)
-    }
-  
-    render() {
-      let {parentViewStyle,backgroundBarStyle,arrayOfProgressObjects} = this.props
-      let onPress = this.props.onPress ? this.props.onPress : null
-      let isDisabled = !this.props.isDisabled && this.props.onPress ? false : true
-      return (
-        <View style={[styles.parentView,parentViewStyle]}>
-          <TouchableOpacity style={[styles.bar,backgroundBarStyle]} onPress={onPress} disabled={isDisabled}>
-            {arrayOfProgressObjects.map(this.getSingleProgressBar)}
-          </TouchableOpacity>
-        </View>
-      );
-    }
-  }
-  ProgressBar.propTypes = {
-    parentViewStyle: ViewPropTypes.style,
-    backgroundBarStyle: ViewPropTypes.style,
-    onStartProgressStyle: PropTypes.shape({
-      borderTopLeftRadius: PropTypes.number,
-      borderBottomLeftRadius: PropTypes.number,
-    }),
-    onEndProgressStyle: PropTypes.shape({
-      borderTopRightRadius: PropTypes.number,
-      borderBottomRightRadius: PropTypes.number,
-    }),
-    arrayOfProgressObjects: PropTypes.arrayOf(PropTypes.object).isRequired,
-    onPress: PropTypes.func,
-    textStyle: Text.propTypes.style
-  }
-  
-  
+const ProgressBar = ({
+  arrayOfProgressObjects,
+  parentViewStyle,
+  backgroundBarStyle,
+  ...props
+}) => {
+  const [total, setTotal] = React.useState(0);
+
+  let onPress = props.onPress ? props.onPress : null;
+  let isDisabled = !props.isDisabled && props.onPress ? false : true;
+
+  React.useEffect(() => {
+    setTotal(getCoveredBar());
+  }, [arrayOfProgressObjects]);
+
+  const getCoveredBar = () => {
+    let readings = arrayOfProgressObjects;
+    return readings.reduce((total, bar) => {
+      return total + bar.value;
+    }, 0);
+  };
+
+  const getSingleProgressBar = (item, index) => {
+    let {
+      onStartProgressStyle,
+      onEndProgressStyle,
+      arrayOfProgressObjects,
+      textStyle,
+    } = props;
+    let totalProgress = parseInt("" + total);
+    return (
+      <SingleBar
+        key={"" + index}
+        {...item}
+        onEndProgressStyle={onEndProgressStyle}
+        onStartProgressStyle={onStartProgressStyle}
+        textStyle={textStyle}
+        isLast={
+          arrayOfProgressObjects?.length == index + 1 && totalProgress >= 1
+        }
+        index={index}
+      />
+    );
+  };
+
+  return (
+    <View style={[styles.parentView, parentViewStyle]}>
+      <TouchableOpacity
+        style={[styles.bar, backgroundBarStyle]}
+        onPress={onPress}
+        disabled={isDisabled}
+      >
+        {arrayOfProgressObjects.map(getSingleProgressBar)}
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+ProgressBar.propTypes = {
+  parentViewStyle: ViewPropTypes.style,
+  backgroundBarStyle: ViewPropTypes.style,
+  onStartProgressStyle: PropTypes.shape({
+    borderTopLeftRadius: PropTypes.number,
+    borderBottomLeftRadius: PropTypes.number,
+  }),
+  onEndProgressStyle: PropTypes.shape({
+    borderTopRightRadius: PropTypes.number,
+    borderBottomRightRadius: PropTypes.number,
+  }),
+  arrayOfProgressObjects: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onPress: PropTypes.func,
+  textStyle: Text.propTypes.style,
+};
